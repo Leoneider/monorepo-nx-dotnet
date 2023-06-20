f0035aa23dc2dd7b2da65d52ca526be79fdd1da9
DevOps42 <leoneider42@gmail.com>
2023-06-20 14:08:03 -0500

    wip: :construction: test affected

--- packages/impares/src/impares/Class2.cs
+++ packages/impares/src/impares/Class2.cs
@@ -6,7 +6,7 @@ public static class Class2
 {
     public static bool EsImPar(int number)
     {
-        if (!Class1.EsPar(number))
+        if (Class1.EsPar(number))
         {
             return true;
 
